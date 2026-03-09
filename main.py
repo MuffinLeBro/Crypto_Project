be35a4820de2e54040fa25536d58a50c8368cece
Beinjamin <jauresbeinjamin@gmail.com>
2026-03-09 11:39:13 +0100

    gestion feature encoder message isc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Client import Client
+from MessageHandler import MessageHandler
 import time
 ADDRSERVER = "vlbelintrocrypto.hevs.ch"
 PORT = 6000
@@ -6,12 +7,19 @@ def main():
    client = Client()
    client.connect(ADDRSERVER, PORT)
    client.start_receiving()
-   message="Je vais prendre une tasse de cafe bien chaud sur scala"
-   print("Message to send: ", message)
-   client.send(message)
-
-   time.sleep(4000000)  # Attendre un peu pour recevoir les messages du serveur
-   client.close()
+   message=["Je vais prendre une tasse de cafe bien chaud sur scala",
+            "Je vais prendre une tasse de cafe bien chaud sur java",
+            "Je vais prendre une tasse de cafe bien chaud sur php",
+            "Je vais prendre une tasse de cafe bien chaud sur ruby",
+            "Je vais prendre une tasse de cafe bien chaud sur scala",]
+   handler = MessageHandler()
+   for msg in message:
+         handler.add_data("s", msg)
+         binaryMessage = handler.get_message()
+         print("Message binaire à envoyer : ", msg)
+         client.send(binaryMessage)
+         time.sleep(1)
+         time.sleep(10)
 
 if __name__ == "__main__":
         main()
