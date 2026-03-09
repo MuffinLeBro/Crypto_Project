9885e7bc9830ab732fed70be11761d9e9ec7455c
Beinjamin <jauresbeinjamin@gmail.com>
2026-03-09 10:40:56 +0100

    add feature gestion multi message

--- Client.py
+++ Client.py
@@ -1,32 +1,44 @@
 import socket
+import threading
 ADDRSERVER = "vlbelintrocrypto.hevs.ch"
 PORT = 6000
+
 class Client :
     def __init__(self,):
        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+       self.running = True     
+
     def connect(self,IP_adress, port):
         try :
-            self.sock.connect(ADDRSERVER, PORT)
+            self.sock.connect((ADDRSERVER, PORT))
             print(f"Connecté à {ADDRSERVER} : {PORT}")
         except Exception as e :
             print("Erreur de connexion à : ", ADDRSERVER)
 
     def send(self,message):
         try : 
+            if isinstance(message, str):
+                message = message.encode('utf-8')
             self.sock.sendall(message)
+            print("Message envoyé")
         except Exception as e :
             print("Erreur de lors de l'envoi : ", e)
 
-    def receive(self, Timeout = 0.001):
-        self.sock.settimeout(Timeout)
-        try : 
-            response = self.receive(4096)
-            return response
-        except Exception as e :
-            print(f"Erreur lors de la reception : ", e)
-            return None
-        
+    def receive(self):
+        try:
+            while self.running:
+                data = self.sock.recv(4096)
+                if not data:
+                    break
+                print("Message reçu : ", data.decode('utf-8'))
+        except Exception as e:
+            print("Erreur lors de la reception : ", e)
+
+    def start_receiving(self):
+               tread = threading.Thread(target=self.receive, daemon=True)
+               tread.start()
     def close(self):
+        self.running = False
         self.sock.close()
         print("Connexion fermée")
         pass
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from Client import Client
+import time
 ADDRSERVER = "vlbelintrocrypto.hevs.ch"
 PORT = 6000
 def main():
    client = Client()
    client.connect(ADDRSERVER, PORT)
-   messagae="Je suis etudient a la heso"
-   print("Message to send: ", messagae)
-   client.send(messagae)
+   client.start_receiving()
+   message="Je vais prendre une tasse de cafe bien chaud sur scala"
+   print("Message to send: ", message)
+   client.send(message)
+
+   time.sleep(4000000)  # Attendre un peu pour recevoir les messages du serveur
+   client.close()
+
 if __name__ == "__main__":
         main()
